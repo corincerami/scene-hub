498ac2ff35abdca832c5d171239b4b2d3c1942fe
Chris C Cerami <chrisccerami@gmail.com>
2014-12-09 17:37:38 -0500

    Added new migration to create band table and created band model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209222506) do
+ActiveRecord::Schema.define(version: 20141209222843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: true do |t|
+    t.string "name", null: false
+  end
 
   create_table "shows", force: true do |t|
     t.text     "details"
