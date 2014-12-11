448b39db68b98d16d24ce2bd2fac43f9884a1679
Chris C Cerami <chrisccerami@gmail.com>
2014-12-11 16:13:18 -0500

    Fixed a bug where new shows could not be added, because the venue_id had been added to the show table with a non null constraint, but the show#create action wasn't updated to add the venue_id to shows. Also added minor styling changes to the flash notices so they weren't inline with the 'Post a new show' button.

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -11,17 +11,16 @@ class ShowController < ApplicationController
   end
 
   def create
-    @show = Show.new(datetime: params[:datetime], details: params[:details])
+    venue = Venue.find_or_create_by(name: params[:venue_name], description: params[:venue_description],
+                                    street_1: params[:venue_street_1], street_2: params[:venue_street_2],
+                                    city: params[:venue_city], state: params[:venue_state],
+                                    zip_code: params[:venue_zip_code])
+    @show = Show.new(datetime: params[:datetime], details: params[:details], venue_id: venue.id)
     if @show.save
       band = Band.find_or_create_by(name: params[:band_name])
       ShowBand.find_or_create_by(band_id: band.id, show_id: @show.id)
       genre = Genre.find_or_create_by(genre: params[:band_genre])
       GenreBand.find_or_create_by(band_id: band.id, genre_id: genre.id)
-      venue = Venue.find_or_create_by(name: params[:venue_name], description: params[:venue_description],
-                                      street_1: params[:venue_street_1], street_2: params[:venue_street_2],
-                                      city: params[:venue_city], state: params[:venue_state],
-                                      zip_code: params[:venue_zip_code])
-      VenueShow.find_or_create_by(venue_id: venue.id, show_id: @show.id)
       redirect_to '/shows', notice: "Show created!"
     else
       render :new
