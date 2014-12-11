7792203ddd7f7698354729c0f18e857f850110aa
Chris C Cerami <chrisccerami@gmail.com>
2014-12-10 19:25:00 -0500

    Shows can now be created with bands and venues that were not already in the database. The form to create a new show now includes sections for band and venue details. These entries will only be added to the database if they don't already exist.

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -10,10 +10,15 @@ class ShowController < ApplicationController
   def create
     @show = Show.new(datetime: params[:datetime], details: params[:details])
     if @show.save
-      band = Band.find_by(name: params[:band_name])
-      ShowBand.create(band_id: band.id, show_id: @show.id)
-      venue = Venue.find_by(name: params[:venue_name])
-      VenueShow.create(venue_id: venue.id, show_id: @show.id)
+      band = Band.find_or_create_by(name: params[:band_name])
+      ShowBand.find_or_create_by(band_id: band.id, show_id: @show.id)
+      genre = Genre.find_or_create_by(genre: params[:band_genre])
+      GenreBand.find_or_create_by(band_id: band.id, genre_id: genre.id)
+      venue = Venue.find_or_create_by(name: params[:venue_name], description: params[:venue_description],
+                                      street_1: params[:venue_street_1], street_2: params[:venue_street_2],
+                                      city: params[:venue_city], state: params[:venue_state],
+                                      zip_code: params[:venue_zip_code])
+      VenueShow.find_or_create_by(venue_id: venue.id, show_id: @show.id)
       redirect_to '/shows', notice: "Show created!"
     else
       render :new
